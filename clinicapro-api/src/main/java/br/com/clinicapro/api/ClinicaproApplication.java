4159ee9feb693d8a074900fe6fab59d94fff34c3
Matheus Maske <matheus.maske.m@gmail.com>
2024-09-02 21:54:10 -0300

    subindo projetos base

---
+++ clinicapro-api/src/main/java/br/com/clinicapro/api/ClinicaproApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinicapro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaproApplication.class, args);
+	}
+
+}
---
+++ clinicapro-api/src/test/java/br/com/clinicapro/api/ClinicaproApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinicapro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

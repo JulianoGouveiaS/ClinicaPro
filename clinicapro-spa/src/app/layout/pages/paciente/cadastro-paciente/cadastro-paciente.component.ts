bad0412b1ae7914f8ad0912055dda44f9bbf8d83
Juliano Gouveia <julianogs@peixoto.com.br>
2024-09-15 01:47:04 -0300

    telas paciente

--- clinicapro-spa/src/app/layout/pages/paciente/cadastro-paciente/cadastro-paciente.component.ts
+++ clinicapro-spa/src/app/layout/pages/paciente/cadastro-paciente/cadastro-paciente.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CalendarModule} from "primeng/calendar";
 import {InputNumberModule} from "primeng/inputnumber";
 import {InputTextModule} from "primeng/inputtext";
+import {CoreComponentsModule} from "../../../../core/components/core-components.module";
+import {ToolbarModule} from "primeng/toolbar";
+import {FormsModule} from "@angular/forms";
+import {Paciente} from "../../../../core/models/paciente";
+import {PacienteService} from "../../../../core/services/paciente.service";
+import {Endereco} from "../../../../core/models/Endereco";
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -9,11 +15,29 @@ import {InputTextModule} from "primeng/inputtext";
     imports: [
         CalendarModule,
         InputNumberModule,
-        InputTextModule
+        InputTextModule,
+        CoreComponentsModule,
+        ToolbarModule,
+        FormsModule
     ],
   templateUrl: './cadastro-paciente.component.html',
   styleUrl: './cadastro-paciente.component.scss'
 })
-export class CadastroPacienteComponent {
+export class CadastroPacienteComponent implements OnInit  {
+    paciente: Paciente = new Paciente();
 
+
+    constructor(private pacienteService: PacienteService) {}
+
+    ngOnInit(): void {
+        this.paciente.endereco = new Endereco();
+    }
+
+    limpar(){
+
+    }
+
+    salvar(){
+        let usuarioSalvo = this.pacienteService.salvar(this.paciente)
+    }
 }

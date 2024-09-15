ac52e9f4f88ca8b782491baa5a73dd1c07836def
Juliano Gouveia <julianogs@peixoto.com.br>
2024-09-14 23:20:53 -0300

    removendo

--- clinicapro-api/src/main/resources/db/migration/V3__tpaciente.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pessoa (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
-    data_nascimento DATE,
-    documento VARCHAR(14),
-    email VARCHAR(255),
-    telefone VARCHAR(255),
-    data_cadastro TIMESTAMP
-);

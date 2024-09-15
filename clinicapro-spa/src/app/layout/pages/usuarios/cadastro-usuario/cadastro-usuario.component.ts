a0e342ead59c85a44772f3919a2ac553cbce9bb1
Matheus Maske <matheus.maske.m@gmail.com>
2024-09-14 23:58:01 -0300

    botao voltar tela cadastro usuario

--- clinicapro-spa/src/app/layout/pages/usuarios/cadastro-usuario/cadastro-usuario.component.ts
+++ clinicapro-spa/src/app/layout/pages/usuarios/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { CoreComponentsModule } from 'src/app/core/components/core-components.module';
 
 @Component({
   selector: 'app-cadastro-usuario',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarModule,
+    CoreComponentsModule,
+    DividerModule,
+    CardModule,
+    TableModule,
+  ],
   templateUrl: './cadastro-usuario.component.html',
   styleUrl: './cadastro-usuario.component.scss'
 })

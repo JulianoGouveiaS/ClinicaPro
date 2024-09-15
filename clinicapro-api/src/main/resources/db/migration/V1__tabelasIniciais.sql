3dd327152d3f2e0f74274106633545915b9f2245
Juliano Gouveia <julianogs@peixoto.com.br>
2024-09-15 01:45:16 -0300

    adicionando novas tabelas

--- clinicapro-api/src/main/resources/db/migration/V1__tabelasIniciais.sql
+++ clinicapro-api/src/main/resources/db/migration/V1__tabelasIniciais.sql
@@ -1,51 +1,72 @@
-CREATE TABLE permissao (
-    id SERIAL PRIMARY KEY,
-    descricao VARCHAR(255),
+CREATE TABLE permissao
+(
+    id                SERIAL PRIMARY KEY,
+    descricao         VARCHAR(255),
     permissao_publica BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE pessoa (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
+CREATE TABLE pessoa
+(
+    id              SERIAL PRIMARY KEY,
+    nome            VARCHAR(255),
     data_nascimento DATE,
-    documento VARCHAR(14),
-    email VARCHAR(255),
-    telefone VARCHAR(255),
-    data_cadastro TIMESTAMP
+    documento       VARCHAR(14),
+    email           VARCHAR(255),
+    telefone        VARCHAR(255),
+    data_cadastro   TIMESTAMP
 );
 
-CREATE TABLE usuario (
-    id SERIAL PRIMARY KEY,
-    id_pessoa BIGINT REFERENCES pessoa(id),
-    login VARCHAR(255),
-    senha VARCHAR(500),
-    ativo BOOLEAN DEFAULT FALSE,
-    ultimo_login TIMESTAMP,
+CREATE TABLE usuario
+(
+    id                 SERIAL PRIMARY KEY,
+    id_pessoa          BIGINT REFERENCES pessoa (id),
+    login              VARCHAR(255),
+    senha              VARCHAR(500),
+    ativo              BOOLEAN DEFAULT FALSE,
+    ultimo_login       TIMESTAMP,
     usuario_temporario BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE permissao_usuario (
-    id SERIAL PRIMARY KEY,
-    id_usuario BIGINT REFERENCES usuario(id),
-    id_permissao BIGINT REFERENCES permissao(id)
+CREATE TABLE permissao_usuario
+(
+    id           SERIAL PRIMARY KEY,
+    id_usuario   BIGINT REFERENCES usuario (id),
+    id_permissao BIGINT REFERENCES permissao (id)
 );
 
-CREATE TABLE recuperacao_senha (
-    id SERIAL PRIMARY KEY,
-    id_pessoa BIGINT REFERENCES pessoa(id),
-    login VARCHAR(255),
-    token VARCHAR(5),
+CREATE TABLE recuperacao_senha
+(
+    id               SERIAL PRIMARY KEY,
+    id_pessoa        BIGINT REFERENCES pessoa (id),
+    login            VARCHAR(255),
+    token            VARCHAR(5),
     data_confirmacao TIMESTAMP
 );
 
-CREATE TABLE endereco (
-    id SERIAL PRIMARY KEY,
-    id_pessoa BIGINT REFERENCES pessoa(id),
-    logradouro VARCHAR(255) NOT NULL,
-    cidade VARCHAR(255),
-    estado VARCHAR(255),
-    bairro VARCHAR(255),
-    numero VARCHAR(255),
+CREATE TABLE endereco
+(
+    id          SERIAL PRIMARY KEY,
+    id_pessoa   BIGINT REFERENCES pessoa (id),
+    logradouro  VARCHAR(255) NOT NULL,
+    cidade      VARCHAR(255),
+    estado      VARCHAR(255),
+    bairro      VARCHAR(255),
+    numero      VARCHAR(255),
     complemento VARCHAR(255),
-    referencia VARCHAR(255)
+    referencia  VARCHAR(255)
+);
+CREATE TABLE Paciente
+(
+    id           SERIAL PRIMARY KEY,
+    tipoCaptacao VARCHAR(255),
+    FOREIGN KEY (id) REFERENCES Pessoa (id)
+);
+
+CREATE TABLE FamiliarPaciente
+(
+    id             SERIAL PRIMARY KEY,
+    nome           VARCHAR(255),
+    relacionamento VARCHAR(255),
+    idPaciente     BIGINT,
+    FOREIGN KEY (idPaciente) REFERENCES Paciente (id)
 );
